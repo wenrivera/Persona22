31193fd762d57d228320630b3c1074744dbb1d54
wendy <wendyca@cantv.net>
2015-04-27 23:28:48 +0200

    inicializacion22

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_4_4_X
+{
+    class Persona22
+    {
+        public Persona22(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.altura22 = altura;
+            this.colorOjos22 = colorOjos;
+            this.colorPelo22 = colorPelo;
+            this.peso22 = peso;
+        }
+
+        public int peso22;
+        public int altura22;
+        public string colorOjos22;
+        public string colorPelo22;
+
+        public void PonerLentillasX(string colorLentillas)
+        {
+
+        }
+
+        public void EngordarX(int cantidad)
+        {
+
+        }
+
+        public void AdelgazarX(double cantidad)
+        {
+
+        }
+    }
+}
